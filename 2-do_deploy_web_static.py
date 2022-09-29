28b661b5585df06558595bf4d7a0625f74e0e85e
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-30 01:49:52 +0300

    update file 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["35.237.92.54", "3.238.90.226"]
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_deploy(archive_path):
